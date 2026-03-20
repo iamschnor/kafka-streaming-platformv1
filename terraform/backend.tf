ccb1524418d4e2a90908922edb7d6a7b98ad5efa
iamschnor <iamschnor237@gmail.com>
2026-03-20 18:15:00 -0500

    Updated terraform file and resources

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "kafka-demo-smveimba-001""
+    bucket = "kafka-terraform-state-bucket1"
     key    = "terraform/state"
     region = "us-east-1"
   }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,98 @@
 resource "aws_s3_bucket" "test_bucket" {
   bucket = "kafka-demo-smveimba-001"
 }
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "kafka-vpc"
+  }
+}
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "kafka-igw"
+  }
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_security_group" "kafka_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  # SSH access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Kafka port
+  ingress {
+    from_port   = 9092
+    to_port     = 9092
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "kafka-security-group"
+  }
+}
+resource "aws_instance" "kafka_server" {
+  count         = 1
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t3.micro"
+
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.kafka_sg.id]
+  key_name               = "kafka-key"
+
+  tags = {
+    Name = "kafka-server-${count.index}"
+  }
+}
